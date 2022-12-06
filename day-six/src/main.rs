6653c9452e19b767816be5bbcc0778520b41a83f
Kaenigsion <herklotzpaul@gmail.com>
2022-12-06 18:05:47 +0100

    day-six

--- day-six/src/main.rs
+++ day-six/src/main.rs
@@ -10,11 +10,11 @@ fn main() {
 fn get_marker(content: String) -> Option<usize> {
     let mut marker = "".to_string();
     for (index, character) in content.chars().enumerate() {
-        if let Some(position_same_character) = marker.chars().position(|x| x == character) {
-            marker.drain(..=position_same_character);
-        }
         if marker.len() == 14 {
             return Some(index);
+        }
+        if let Some(position_same_character) = marker.chars().position(|x| x == character) {
+            marker.drain(..=position_same_character);
         }
         marker.push(character);
     }
